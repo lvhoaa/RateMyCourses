4f1d2ea092fee0869ed536035a06f51922980d3a
prohack17 <lavanhoa41@gmail.com>
2023-12-22 16:12:29 -0500

    add google oauth

--- ratemycourse/urls.py
+++ ratemycourse/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('register/',views.register),
     path('login/',auth_views.LoginView.as_view(template_name='registration/login.html',next_page='/')),
     path('logout/',auth_views.LogoutView.as_view(template_name='registration/logout.html',next_page='/')),
+    path('accounts/',include('allauth.urls'))
 ]
